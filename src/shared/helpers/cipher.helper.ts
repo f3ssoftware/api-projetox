fffe13bff6f6b0cb9b5a803b5b72169c8e938e37
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2023-04-03 11:47:08 -0300

    removing unused dependency

--- src/shared/helpers/cipher.helper.ts
+++ src/shared/helpers/cipher.helper.ts
@@ -1,12 +1,12 @@
-import * as CryptoJS from 'crypto-js';
+// import * as CryptoJS from 'crypto-js';
 
-const encrypt = (word: string) => {
-  return CryptoJS.AES.encrypt(word, 'pagstar').toString();
-};
+// const encrypt = (word: string) => {
+//   return CryptoJS.AES.encrypt(word, 'pagstar').toString();
+// };
 
-const decrypt = (word: string) => {
-  const bytes = CryptoJS.AES.decrypt(word, 'pagstar');
-  return bytes.toString(CryptoJS.enc.Utf8);
-};
+// const decrypt = (word: string) => {
+//   const bytes = CryptoJS.AES.decrypt(word, 'pagstar');
+//   return bytes.toString(CryptoJS.enc.Utf8);
+// };
 
-export { encrypt, decrypt };
+// export { encrypt, decrypt };
