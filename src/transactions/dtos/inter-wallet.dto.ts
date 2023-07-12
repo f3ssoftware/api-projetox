5268e2481ffe6c01662ef8830da11fe42ed3b3fb
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2023-07-12 20:27:37 -0300

    fixing interwallet endpoint doc

--- src/transactions/dtos/inter-wallet.dto.ts
+++ src/transactions/dtos/inter-wallet.dto.ts
@@ -1,7 +1,24 @@
-export interface InterWalletDto {
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNumber, IsString, IsUUID } from 'class-validator';
+
+export class InterWalletDto {
+  @ApiProperty()
+  @IsUUID()
   wallet_id: string;
+
+  @ApiProperty()
+  @IsNumber()
   amount: number;
+
+  @ApiProperty()
+  @IsUUID()
   destinatary_wallet_id: string;
+
+  @ApiProperty()
+  @IsString()
   reference: string;
+
+  @ApiProperty()
+  @IsDateString()
   due_date: Date;
 }
